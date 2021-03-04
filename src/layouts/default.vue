4133413403531ded42c6ea3df597d0dba5a704dc
Cedric Texier <cedix@icloud.com>
2021-03-04 17:45:21 +0100

    Fix min height

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,12 +1,14 @@
 <script setup>
-import TheHeader from "../components/header.vue";
+  import TheHeader from '../components/header.vue'
 </script>
 
 <template>
-  <TheHeader />
-  <main class="bg-gray-50">
-    <div class="max-w-5xl m-auto min-h-screen">
-      <router-view />
-    </div>
-  </main>
+  <div class="min-h-screen flex flex-col">
+    <TheHeader />
+    <main class="flex-1 bg-gray-50">
+      <div class="max-w-5xl m-auto">
+        <router-view />
+      </div>
+    </main>
+  </div>
 </template>
