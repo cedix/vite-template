a6979368ab9186c61668f36574387f41fd7e0e36
Cedric Texier <cedix@icloud.com>
2021-03-04 17:56:05 +0100

    Fix layout max-width

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -6,7 +6,7 @@
   <div class="min-h-screen flex flex-col">
     <TheHeader />
     <main class="flex-1 bg-gray-50">
-      <div class="max-w-5xl m-auto">
+      <div class="max-w-7xl m-auto">
         <router-view />
       </div>
     </main>
