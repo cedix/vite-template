1769708cfba87e8f96df136097d74dcc9267f19a
Cedric Texier <cedix@icloud.com>
2021-04-07 16:37:35 +0200

    add new page for composition api examples

---
+++ src/pages/composition.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="p-5 max-w-5xl mx-auto">
+    <div class="flex">
+      <h1 class="text-2xl mb-4">Composition API template</h1>
+    </div>
+    <div>TODO</div>
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: default
+</route>
